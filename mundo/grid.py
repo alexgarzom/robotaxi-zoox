2b844e354218c5cc4e0a9c705f55359c33be6ca2
Estiven <estivenfrv@gmail.com>
2026-03-19 18:19:51 -0500

    Creación del método costo_movimiento, y es transitable que verifica que el agente se pueda movilizar y saber en que tipo de casilla se encuentra

--- mundo/grid.py
+++ mundo/grid.py
@@ -35,5 +35,23 @@ class Grid:
     def es_valida(self,fila,col):   #Verifica si una coordenada está dentro del grid
 
         return 0 <= fila < self.filas and 0 <= col < self.columnas #Dentro de los limites
+    
+    def es_transitable(self,fila,col):   #Verifica si una cleda puede ser transitada, osea que no sea un muro
+        if not self.es_valida(fila,col): #Esta grueda del grid
+            return False                 #No transitable
+        return self.matriz[fila][col] != Grid.MURO     #True si no es un muro
+    
+    def costo_movimiento(self,fila,col):  #Obtiene el costo de estar en una casilla especifica
+        tipo=self.matriz[fila][col]       #Obtiene el tipo de celda
+
+        #Segun el tipo retorna el costo
+
+        if tipo in[Grid.LIBRE,Grid.INICIO,Grid.PASAJERO,Grid.DESTINO]:
+            return 1                                                        #Costo 1 para flujo bajo
+        elif tipo == Grid.FLUJO_ALTO:
+            return 7                                                        #Costo 7 para flujo alto
+        return 1                                                            #Valor por defecto
+
+
 
             
