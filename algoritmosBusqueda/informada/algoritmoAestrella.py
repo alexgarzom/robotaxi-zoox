1c576667a52bf9c6d7a7380de28a95e743241cf9
Estiven <estivenfrv@gmail.com>
2026-03-31 16:36:52 -0500

    Actualización de la clase algoritmoAestrela

--- algoritmosBusqueda/informada/algoritmoAestrella.py
+++ algoritmosBusqueda/informada/algoritmoAestrella.py
@@ -20,6 +20,42 @@ def a_estrella(grid,inicio,destino,pasajeros):    #Algoritmo A* comnbina costo r
         h=heuristica_manhattan(inicio,destino)
     )
 
+    contador = 0
+    frontera = []
+    heapq.heappush(frontera, (nodo_inicial.f, contador, nodo_inicial))
+    contador +=1
+
+    visitados = {}
+    nodos_expandidos=0
+
+    while frontera:
+        _,_, nodo = heapq.heappop(frontera)
+        nodos_expandidos+=1
+
+        if nodo.pasajeros_recogidos==meta_pasajeros and nodo.posicion == destino:
+            return {
+                'camino': nodo.obtener_camino(),
+                'costo':nodo.g,
+                'nodos_expandidos': nodos_expandidos,
+                'profundidad':nodo.profundidad
+            }
+        
+        estado = (nodo.posicion, nodo.pasajeros_recogidos)
+
+        if estado in visitados and visitados[estado] <= nodo.g:
+            continue
+        visitados[estado] = nodo.g
+
+        for vecino in grid.get_vecinos(nodo):
+            vecino.h=heuristica_manhattan(vecino.posicion, destino)
+            vecino.f=vecino.g + vecino.h
+            heapq.heappush(frontera, (vecino.f, contador, vecino))
+            contador +=1
+
+            return None
+
+
+        
 
 
     
